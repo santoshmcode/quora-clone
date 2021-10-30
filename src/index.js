d67ecb89d38aff7e300f5cc7f0ee1ca41cae646a
Santosh Mane <santoshmane619@gmail.com>
2021-10-31 02:14:18 +0530

    added global styling

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { GlobalStyles } from "./styles/GlobalStyle";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
+            <GlobalStyles />
             <App />
         </Provider>
     </React.StrictMode>,
