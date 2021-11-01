b5fa73cebbafed5e1152d070ab3658e23c06e35f
asutoshb <beheraasutosh132@gmail.com>
2021-11-01 23:39:05 +0530

    added answer share icons at top

--- src/components/post-page/PostPage.jsx
+++ src/components/post-page/PostPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './PostPage.css';
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import {FiEdit2, FiRss} from 'react-icons/fi'
+import {AiOutlineScan} from 'react-icons/ai'
+import {VscAccount} from 'react-icons/vsc'
+import {FaRegComment} from 'react-icons/fa'
+import {IoPersonOutline} from 'react-icons/io5'
+import {IoEllipsisHorizontalOutline, IoArrowRedoOutline, IoArrowDownSharp} from 'react-icons/io5'
+
 
 export const PostPage = () => {
     return (
@@ -12,12 +18,39 @@ export const PostPage = () => {
                     <div>Opinions</div>
                     <div>Perspective</div>
                     <div>India</div>
-                    <EditOutlinedIcon></EditOutlinedIcon>
+                    <FiEdit2></FiEdit2>
                 </div>
 
                 <div className="left-title">
                     How is India Viewed From Outside?
                 </div>
+
+                <div className="left-answer-icon">
+                    <div className="answer-icon">
+                        <div className="answer-icon-inner">
+                            <div><AiOutlineScan></AiOutlineScan></div>
+                            <div>Answer</div>
+                        </div>
+                        <div className="answer-icon-inner">
+                            <div><FiRss></FiRss></div>
+                            <div>Follow</div>
+                        </div>
+                       <div className="answer-icon-inner">
+                            <div><IoPersonOutline></IoPersonOutline></div>
+                            <div>Request</div>
+                        </div>
+                    </div>
+                    <div className="share-icon">
+                        <FaRegComment></FaRegComment>
+                        <IoArrowDownSharp></IoArrowDownSharp>
+                        <IoEllipsisHorizontalOutline></IoEllipsisHorizontalOutline>
+                        <IoArrowRedoOutline></IoArrowRedoOutline>
+                    </div>
+                    
+                </div>
+
+                <hr />
+
             </div>
             
             <div>
