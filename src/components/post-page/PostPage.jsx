e4508cb268569ea767e7f3889b6e02d1d1261633
asutoshb <beheraasutosh132@gmail.com>
2021-11-02 15:59:44 +0530

    added sidebar questions

--- src/components/post-page/PostPage.jsx
+++ src/components/post-page/PostPage.jsx
@@ -142,7 +142,16 @@ Upon successful verification of transactions, these transactions are grouped and
             </div>
             
             <div className="outer-right">
-               
+                <div className="right-head">Related Questions</div>
+                <hr />
+                <div className="right-body">Did you ever get a request for a favor from a relative who never showed any respect or</div>
+                <div className="right-body">What did someone say to you that instantly made you realize their life was in danger?</div>
+                <div className="right-body">Have you ever lost any respect for someone instantly?</div>
+                <div className="right-body">What made you respect someone today?</div>
+                <div className="right-body">Who has totally lost your respect? Why?</div>
+                <div className="right-body">How can you make others respect you?</div>
+
+                <div className="right-footer">Ask a question</div>
             </div>
         </div>
     </div>
