05919f1dcdadd3cebf2ef531e59a277530d11149
Pranali-5 <malkarpg@gmail.com>
2021-11-03 19:45:33 +0530

    navbar

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -9,42 +9,46 @@ import { auth } from "../config/firebase.config";
 import { PostPage } from "../components/post-page/PostPage";
 
 import { HomeMain } from "../components/HomeContents/HomeMain";
+import { Navbar } from "../components/Navbar/Navbar";
 
 const Router = () => {
-    const user = useSelector(selectUser);
-    const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                dispatch(login(user));
-            } else {
-                dispatch(logout());
-            }
-        });
-    }, [dispatch]);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(login(user));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
-    return (
-        <Switch>
-            <Route path="/" exact>
-                {user ? (
-                    <>
-                        <h1>Home page</h1>
-                        <p>Hello {user.email}</p>
-                        <button onClick={() => auth.signOut()}>Logout</button>
-                    </>
-                ) : (
-                    <Login />
-                )}
-            </Route>
-            <Route exact path="/postPage">
-                <PostPage />
-            </Route>
-            <Route path="/homemaincontainer">
-                <HomeMain />
-            </Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/" exact>
+        {user ? (
+          <>
+            <h1>Home page</h1>
+            <p>Hello {user.email}</p>
+            <button onClick={() => auth.signOut()}>Logout</button>
+          </>
+        ) : (
+          <Login />
+        )}
+      </Route>
+      <Route exact path="/postPage">
+        <PostPage />
+      </Route>
+      <Route path="/homemaincontainer">
+        <HomeMain />
+      </Route>
+      <Route path="/homepage">
+        <Navbar />
+      </Route>
+    </Switch>
+  );
 };
 
 export default Router;
