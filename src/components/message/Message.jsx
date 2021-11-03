35ee5405d299ba7f172faa68c1de710945498331
asutoshb <beheraasutosh132@gmail.com>
2021-11-03 16:51:18 +0530

    added some style to message pop up

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -30,8 +30,8 @@ export const Message = () => {
                 </MessageContInner>
             </MessageCont>
 
-            <MessageChat style={{display: dis}} onClick={handleChat}>
-                 <MessageContInner>
+            <MessageChat style={{display: dis}}>
+                 <MessageContInner onClick={handleChat}>
                     <MessageTitle>Messages</MessageTitle>
                     <MessageIcon>
                         <div><svg width="24" height="24" viewBox="0 0 24 24" fill="none"><path class="icon_svg-fill_as_stroke" d="M4.5 6.5V5.75C4.08579 5.75 3.75 6.08579 3.75 6.5H4.5ZM19.5 6.5H20.25C20.25 6.08579 19.9142 5.75 19.5 5.75V6.5ZM19.5 18.5V19.25C19.9142 19.25 20.25 18.9142 20.25 18.5H19.5ZM3.75 13C3.75 13.4142 4.08579 13.75 4.5 13.75C4.91421 13.75 5.25 13.4142 5.25 13H3.75ZM10 17.75C9.58579 17.75 9.25 18.0858 9.25 18.5C9.25 18.9142 9.58579 19.25 10 19.25V17.75ZM4.5 7.25H19.5V5.75H4.5V7.25ZM18.75 6.5V18.5H20.25V6.5H18.75ZM5.25 13V6.5H3.75V13H5.25ZM19.5 17.75H10V19.25H19.5V17.75Z" fill="#666666"></path><path class="icon_svg-stroke" d="M4.5 7.5L12 14L19.5 7.5" stroke="#666666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path class="icon_svg-stroke" d="M7 18.5H2M4.5 16V21" stroke="#666666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>
@@ -46,9 +46,14 @@ export const Message = () => {
                         <p>No Messages</p>
                         <span>Connect with others on Quora by beginning a new conversation.</span>
                     </div>
-                    
-                    
                 </Image>
+
+                <Button>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <div><svg width="24" height="24" viewBox="0 0 24 24" fill="none"><path class="icon_svg-fill_as_stroke" d="M4.5 6.5V5.75C4.08579 5.75 3.75 6.08579 3.75 6.5H4.5ZM19.5 6.5H20.25C20.25 6.08579 19.9142 5.75 19.5 5.75V6.5ZM19.5 18.5V19.25C19.9142 19.25 20.25 18.9142 20.25 18.5H19.5ZM3.75 13C3.75 13.4142 4.08579 13.75 4.5 13.75C4.91421 13.75 5.25 13.4142 5.25 13H3.75ZM10 17.75C9.58579 17.75 9.25 18.0858 9.25 18.5C9.25 18.9142 9.58579 19.25 10 19.25V17.75ZM4.5 7.25H19.5V5.75H4.5V7.25ZM18.75 6.5V18.5H20.25V6.5H18.75ZM5.25 13V6.5H3.75V13H5.25ZM19.5 17.75H10V19.25H19.5V17.75Z" fill="white"></path><path class="icon_svg-stroke" d="M4.5 7.5L12 14L19.5 7.5" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path class="icon_svg-stroke" d="M7 18.5H2M4.5 16V21" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>
+                        <div style={{marginLeft:'3px', marginTop:'4px'}}> New Messages</div>
+                    </div>
+                </Button>
 
             </MessageChat>
 
@@ -100,14 +105,14 @@ const MessageCont = styled.div`
 
  const MessageChat = styled.div`
     width: 350px;
-    height: 60vh;
+    height: 57vh;
     border: 1px solid #f4f0f0;
     position : fixed;
     right: 30px;
     bottom : 0;
     z-index: 1000;
     hr{
-        border: 1px solid #f4f0f0;
+        border-top: 0.1px solid #f4f0f0;
     }
  `
 
@@ -134,5 +139,13 @@ const MessageCont = styled.div`
         padding-right: auto;
     }
  `
-
+const Button = styled.button`
+    width: 300px;
+    height: 40px;
+    border-radius: 20px;
+    margin-left: 25px;
+    background-color: blue;
+    color: white;
+    margin-top: 200px;
+`
  
