be74458c82075557e6d1480b0cd1bd830594962e
asutoshb <beheraasutosh132@gmail.com>
2021-11-12 20:53:19 +0530

    add padding to editor

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import styled from "styled-components";
+import {Link} from 'react-router-dom'
 
 export const Message = () => {
 
@@ -24,7 +25,7 @@ export const Message = () => {
                 <MessageContInner>
                     <MessageTitle>Messages</MessageTitle>
                     <MessageIcon>
-                        <div><svg width="24" height="24" viewBox="0 0 24 24" fill="none"><path class="icon_svg-fill_as_stroke" d="M4.5 6.5V5.75C4.08579 5.75 3.75 6.08579 3.75 6.5H4.5ZM19.5 6.5H20.25C20.25 6.08579 19.9142 5.75 19.5 5.75V6.5ZM19.5 18.5V19.25C19.9142 19.25 20.25 18.9142 20.25 18.5H19.5ZM3.75 13C3.75 13.4142 4.08579 13.75 4.5 13.75C4.91421 13.75 5.25 13.4142 5.25 13H3.75ZM10 17.75C9.58579 17.75 9.25 18.0858 9.25 18.5C9.25 18.9142 9.58579 19.25 10 19.25V17.75ZM4.5 7.25H19.5V5.75H4.5V7.25ZM18.75 6.5V18.5H20.25V6.5H18.75ZM5.25 13V6.5H3.75V13H5.25ZM19.5 17.75H10V19.25H19.5V17.75Z" fill="#666666"></path><path class="icon_svg-stroke" d="M4.5 7.5L12 14L19.5 7.5" stroke="#666666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path class="icon_svg-stroke" d="M7 18.5H2M4.5 16V21" stroke="#666666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>
+                        <div><Link to="/chat"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"><path class="icon_svg-fill_as_stroke" d="M4.5 6.5V5.75C4.08579 5.75 3.75 6.08579 3.75 6.5H4.5ZM19.5 6.5H20.25C20.25 6.08579 19.9142 5.75 19.5 5.75V6.5ZM19.5 18.5V19.25C19.9142 19.25 20.25 18.9142 20.25 18.5H19.5ZM3.75 13C3.75 13.4142 4.08579 13.75 4.5 13.75C4.91421 13.75 5.25 13.4142 5.25 13H3.75ZM10 17.75C9.58579 17.75 9.25 18.0858 9.25 18.5C9.25 18.9142 9.58579 19.25 10 19.25V17.75ZM4.5 7.25H19.5V5.75H4.5V7.25ZM18.75 6.5V18.5H20.25V6.5H18.75ZM5.25 13V6.5H3.75V13H5.25ZM19.5 17.75H10V19.25H19.5V17.75Z" fill="#666666"></path><path class="icon_svg-stroke" d="M4.5 7.5L12 14L19.5 7.5" stroke="#666666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path class="icon_svg-stroke" d="M7 18.5H2M4.5 16V21" stroke="#666666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></Link></div>
                         <div><svg width="24px" height="24px" viewBox="0 0 24 24"><g class="icon_svg-stroke" stroke="#666" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round"><polyline transform="translate(12.002415, 12.000000) scale(1, -1) translate(-12.002415, -12.000000) " points="5 8.5 12 15.5 19.0048307 8.5"></polyline></g></svg></div>
                     </MessageIcon>
                 </MessageContInner>
